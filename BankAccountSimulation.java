37474efe0d2956b2fabfae74910d962c93124e75
qyl <104905386+qylq@users.noreply.github.com>
2023-12-23 20:17:00 +0300

    Add files via upload

--- BankAccountSimulation.java
+++ BankAccountSimulation.java
@@ -3,36 +3,52 @@ package inter.qylex.m3.BankAccountSimulation;
 import java.util.Random;
 
 public class BankAccountSimulation {
+    private static int account = 1000;
+    private static Object KEY = new Object();
+    private static final Random random = new Random();
+
     public static void main(String[] args) {
-        BankAccount account = new BankAccount(999);
-
-        Thread depositThread = new Thread(() -> {
-            Random random = new Random();
-            while (!account.flag) {
-                int depositAmount = random.nextInt(333);
-                account.deposit(depositAmount);
+        Thread deposit = new Thread(() -> {
+            while (true) {
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(random.nextInt(1500) + 500);
+                    int amount = random.nextInt(400) + 100;
+                    synchronized (KEY) {
+                        account += amount;
+                        System.out.println("Пополнение на сумму: " + amount + ". Новый баланс: " + account);
+                    }
                 } catch (InterruptedException e) {
                     System.out.println(e.getMessage());
                 }
             }
         });
 
-        Thread withdrawThread = new Thread(() -> {
-            Random random = new Random();
-            while (!account.flag) {
-                int withdrawAmount = random.nextInt(666);
-                account.withdraw(withdrawAmount);
+        Thread withdraw = new Thread(() -> {
+            while (true) {
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(random.nextInt(1500) + 500);
+                    int amount = random.nextInt(700) + 100;
+                    synchronized (KEY) {
+                        if (amount <= account) {
+                            account -= amount;
+                            System.out.println("Снятие на сумму: " + amount + ". Новый баланс: " + account);
+                        } else {
+                            System.out.println("Не хватает!");
+                            break;
+                        }
+                    }
                 } catch (InterruptedException e) {
                     System.out.println(e.getMessage());
                 }
             }
         });
 
-        depositThread.start();
-        withdrawThread.start();
+        new Thread(deposit).start();
+        try {
+            deposit.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        new Thread(withdraw).start();
     }
 }
